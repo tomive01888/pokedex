bffab0ad5e2993e8cd75220d52893a31a3e51a55
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-04-21 14:17:22 +0200

    push

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const pokeapiUrl = "https://pokeapi.co/api/v2/pokemon"
-const genOne = "limit=151"
+const genOne = "?limit=151"
 
 async function fetchGenerationOne() {
     try{
+        const request = await fetch(pokeapiUrl + genOne) 
+        console.log(request.ok)     
+
+        if(!request.ok){
+
+            throw new Error("Noe faen kjeeeddelig")
+
+             
+    
+        }
+
+        const result = await request.json()
+
+        const pokemon = result.results
+
+        console.log(pokemon[100])  
+
+       
+    
         
     }catch(error){
+        console.log("ERROR")
 
     }
 }
+
+fetchGenerationOne()
