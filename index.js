1a42241d46762163019b787c46a9692fa0dd3e5c
petterkoding <69626923+petterkoding@users.noreply.github.com>
2024-04-21 14:05:31 +0200

    add api url

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let name = "hallo der"
+const pokeapiUrl = "https://pokeapi.co/api/v2/pokemon"
+const genOne = "limit=151"
+
+async function fetchGenerationOne() {
+    try{
+        
+    }catch(error){
+
+    }
+}
